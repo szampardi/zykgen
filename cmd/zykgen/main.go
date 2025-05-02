3b43959e15be3d5031813baf0d774d605d1cdd90
szampardi <szampardi@deepnull.com>
2025-05-02 12:45:40 +0200

    auto batch size

--- cmd/zykgen/main.go
+++ cmd/zykgen/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
 	"strconv"
 	"sync"
 	"time"
@@ -46,7 +47,7 @@ func init() {
 		Mojito:       flag.Bool("m", false, "algorithm"),
 		Negroni:      flag.Bool("n", false, "algorithm"),
 		Cosmopolitan: flag.Bool("c", true, "algorithm"),
-		BatchSize:    flag.Int64("b", 375000, "batch size"),
+		BatchSize:    flag.Int64("b", int64(runtime.NumCPU()*25000), "batch size"),
 	}
 	flag.Parse()
 	if *cfg.Mojito {
